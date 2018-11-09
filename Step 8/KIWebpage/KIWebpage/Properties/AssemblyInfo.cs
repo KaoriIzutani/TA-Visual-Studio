73bd88e100cc8ae20c899ee2a2ac15bce0f3ef74
Kaori Izutani <kaoriizutani@yahoo.com>
2018-11-09 15:36:09 -0800

    Final Practical

---
+++ Step 8/KIWebpage/KIWebpage/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KIWebpage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KIWebpage")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9564e01c-e2ed-41aa-a03c-d81e70aa2b8d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Step 8/Math Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This application will add two whole or decimal numbers and display their sum.");
+            Console.WriteLine("Please enter the first whole or decimal number. ");
+            string input = Console.ReadLine();
+
+
+            Console.WriteLine("Please enter the second whole or decimal number. ");
+            string input2 = Console.ReadLine();
+
+            try
+            {
+                var num1 = Convert.ToInt32(input); //Check input for doubles
+                var num2 = Convert.ToInt32(input2); //This avoids some errors
+                getSum(num1, num2);
+
+            }
+            catch
+            {
+                var num1 = Convert.ToDouble(input); //Check input for doubles
+                var num2 = Convert.ToDouble(input2); //This avoids some errors
+                getSum(num1, num2);
+            }
+
+            //This keeps the console open until the user presses Enter to close           
+            Console.WriteLine("Press Enter to close...");
+            Console.ReadLine();
+        }
+
+        public static void getSum(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine("8 + 2 = {0}", result);
+        }
+
+        public static void getSum(double num1, double num2)
+        {
+            double result = num1 + num2;
+            Console.WriteLine("7.4 + 2.6 = {0}", result);
+        }
+
+    }
+}
